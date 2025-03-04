8f60b32bb21a72a1a9dc3dcb0930899ea6a9f98d
Roberto Sanchez <rsanchezs151@gmail.com>
2025-03-03 18:46:50 -0600

    Go routines async

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
-import (
-	//"github.com/rsanchezs151/godesde0/users"
-	e "github.com/rsanchezs151/godesde0/defer_panic"
-)
-
 func main() {
 	// estado, texto := variables.ConviertoaTexto(65465)
 
 	// fmt.Println(estado)
 	// fmt.Println(texto)
 	/*
-		Pedro := new(models.Hombre)
-		e.HumanosRespirando(Pedro)
-		Ana := new(models.Mujer)
-		e.HumanosRespirando(Ana)
-	*/
-	e.VemosDefer()
-	e.EjemploPanic()
+				Pedro := new(models.Hombre)
+				e.HumanosRespirando(Pedro)
+				Ana := new(models.Mujer)
+				e.HumanosRespirando(Ana)
 
+			e.VemosDefer()
+			e.EjemploPanic()
+
+		go e.MiNombreLento("Roberto")
+		fmt.Println("Estoy aqui")
+		var x string
+
+		fmt.Scanln(&x)
+	*/
 }
